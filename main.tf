e9911562c9ec98f9116b0399c83c0eea4d1740d6
Dave Parker <dparker@scalyr.com>
2020-09-03 10:16:20 -0700

    Null-label 0.16.0

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/scalyr/terraform-null-label.git?ref=tags/0.15.0-scalyr"
+  source     = "git::https://github.com/scalyr/terraform-null-label.git?ref=tags/0.16.0-scalyr"
   namespace  = var.namespace
   name       = var.name
   stage      = var.stage
